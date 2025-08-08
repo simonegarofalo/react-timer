2644542c50a4fd63a7b2a21c27fec262f56594ec
s.orlando <salvatore.orlando@digitouch.it>
2025-08-08 10:24:09 +0200

    feat: moved Timer into a specific component aim to reuse it

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,18 @@
-import { useEffect, useState } from "react";
-import { useRef } from "react";
+import Timer from './components/timer.jsx';
 
 import "./App.css";
 
+const ATHLETES = ["Simone", "Salvatore", "Carmelo", "Giancarlo"];
+
 function App() {
-  // State to store the current count
-  const [count, setCount] = useState(0);
-  // State to control whether the timer is running
-  const [isRunning, setIsRunning] = useState(false);
-  // Stores the interval ID to prevent duplication
-  const intervalRef = useRef(null);
-
-  useEffect(() => {
-    // If the timer is running and no interval is already set, start a new interval
-    if (isRunning) {
-      if (intervalRef.current !== null) return;
-      // Set up the interval to increment count every second
-      intervalRef.current = setInterval(() => {
-        setCount((prev) => prev + 0.001);
-      }, 1);
-    } else {
-      // If the timer is paused or reset, clear the interval
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
-    }
-    // Cleanup function
-    return () => {
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
-    };
-  }, [isRunning]); // Dependency array
-
-  return (
+ return (
     <>
-      <h1>{count.toFixed(3)}</h1>
-      <div className="button-wrapper">
-        <button onClick={() => setIsRunning(true)}>Start</button>
-        <button onClick={() => setIsRunning(false)}>Pause</button>
-        <button
-          onClick={() => {
-            setCount(0);
-            setIsRunning(false);
-          }}
-        >
-          Reset
-        </button>
-      </div>
+      <Timer />
+      {ATHLETES.map(athlete => (
+        <Timer athlete={athlete} />
+      ))}
     </>
-  );
+  ) 
 }
 
 export default App;
